a7a23ebe5f1b77d758046f029e60cb9588a881fd
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-29 22:32:32 +0200

    Add list of int to Class1

--- Library/Class1.cs
+++ Library/Class1.cs
@@ -1,6 +1,16 @@
 ﻿namespace Library;
 public class Class1
 {
+    public int[] my_numbers;
+
+    public Class1()
+    {
+        my_numbers = new int[10];
+    }
+    public Class1(int number)
+    {
+        my_numbers = Enumerable.Repeat(number, 10).ToArray();
+    }
     public bool IsIt()
     {
         return true;
--- LibraryTests/UnitTest1.cs
+++ LibraryTests/UnitTest1.cs
@@ -11,4 +11,14 @@ public class UnitTest1
         Class1 myobject = new Class1();
         Assert.IsTrue(myobject.IsIt());
     }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Class1 myobject = new Class1(5);
+        for (int i = 0; i < 10; i++)
+        {
+            Assert.AreEqual(myobject.my_numbers[i], 5);
+        }
+    }
 }
