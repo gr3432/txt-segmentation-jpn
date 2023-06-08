c7b359dd387284f3ad7bb3660e35bd307bbb93a9
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-06-08 11:23:12 +0200

    Remove unused class

--- LibraryTests/UnitTest1.cs
+++ LibraryTests/UnitTest1.cs
@@ -6,38 +6,6 @@ namespace LibraryTests;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
-    {
-        Class1 myobject = new Class1();
-        Assert.IsTrue(myobject.IsIt());
-    }
-
-    [TestMethod]
-    public void TestMethod2()
-    {
-        Class1 myobject = new Class1(5);
-        for (int i = 0; i < Class1.SIZE; i++)
-        {
-            Assert.AreEqual(myobject.my_numbers[i], 5);
-        }
-    }
-
-    [TestMethod]
-    public void TestGetNameProperty()
-    {
-        Class1 myobject = new Class1();
-        Assert.AreEqual(myobject.Name, "no_name");
-    }
-
-    [TestMethod]
-    public void TestSetNameProperty()
-    {
-        Class1 myobject = new Class1();
-        myobject.Name = "some_name";
-        Assert.AreEqual(myobject.Name, "some_name");
-    }
-
-    [TestMethod]
     public void TestConverter()
     {
         string hiragana = "ぴかちゅう";
